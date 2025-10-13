dbaca10c4d324caa68eda6e3957533ffe1bd3685
Ecssooo <alexandre.lejeune9@gmail.com>
2025-10-13 09:41:50 +0200

    [ADD] UnrealProject

---
+++ Source/CouchGameEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class CouchGameEditorTarget : TargetRules
+{
+	public CouchGameEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		ExtraModuleNames.Add("CouchGame");
+	}
+}
