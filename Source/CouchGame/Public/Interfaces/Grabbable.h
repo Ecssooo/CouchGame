1a7a1ed92c39d57f8e679f1117fd712e8dbab193
Ecssooo <alexandre.lejeune9@gmail.com>
2025-11-12 11:38:12 +0100

    [ADD : Socket]

--- Source/CouchGame/Public/Interfaces/Grabbable.h
+++ Source/CouchGame/Public/Interfaces/Grabbable.h
@@ -27,4 +27,7 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void OnDrop(ACharacterPlayer* Player);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	bool GetIsGrabbed();
 };
