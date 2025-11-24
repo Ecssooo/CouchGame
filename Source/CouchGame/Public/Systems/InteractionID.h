80bd9789eedd2de68ef19d4c9ed3bd2a6e4bfca7
Ecssooo <alexandre.lejeune9@gmail.com>
2025-11-24 13:16:06 +0100

    [ADD] Enum Interaction ID

---
+++ Source/CouchGame/Public/Systems/InteractionID.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+UENUM(BlueprintType)
+enum class EInteractionsID : uint8
+{
+	Swing,
+	Fishing,
+	Ball,
+	Tampon,
+	Simon,
+	Kitchen,
+	Sit,
+	Snowball,
+	Hut
+};
