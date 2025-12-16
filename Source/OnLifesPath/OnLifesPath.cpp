58eb7fb022befb3731aab6cc046766574c41c196
Ecssooo <147376957+Ecssooo@users.noreply.github.com>
2025-12-16 11:26:45 +0100

    files names

--- Source/OnLifesPath/OnLifesPath.cpp
+++ Source/OnLifesPath/OnLifesPath.cpp
@@ -1,6 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "CouchGame.h"
+#include "OnLifesPath.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OnLifesPath, "OnLifesPath");
